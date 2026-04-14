1bf4d833e7717c294c7a8a6574dc16d2b039928d
Ahmad Alawneh <aalawneh19@gmail.com>
2026-04-14 20:05:26 +0300

    added ButtonLink component

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -1,8 +1,14 @@
+import { ButtonLink } from '@/components/blocks/button-link';
 import { ProjectGrid } from '@/components/containers/project-grid';
+import { PlusIcon } from 'lucide-react';
 
 export default function Dashboard() {
   return (
-    <div className="p-6">
+    <div className="p-6 pt-0 flex flex-col">
+      <ButtonLink href="/new" size="lg" className="flex gap-2 items-center justify-center self-end mb-6">
+        <span>Add Project</span>
+        <PlusIcon />
+      </ButtonLink>
       <ProjectGrid />
     </div>
   );
